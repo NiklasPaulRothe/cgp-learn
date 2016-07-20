2e884c201b9de9c2d15c6368650569ec67c1a144
NiklasPaulRothe <nrothe@uni-osnabrueck.de>
2016-07-20 11:15:44 +0200

    fixed 1

--- task4/nrothe/suchmagic.rs
+++ task4/nrothe/suchmagic.rs
@@ -1,9 +1,7 @@
- use std::ops::{Mul, Add};
+use std::ops::{Mul, Add};
 
 fn magic<T: Mul + Add + Copy>(par1: T, par2: T) -> (<T as Mul>::Output, <T as Add>::Output) {
-    let p1 = par1;
-    let p2 = par2;
-    (par1 * par2, p1 + p2)
+    (par1 * par2, par1 + par2)
 }
 
 fn main() {
